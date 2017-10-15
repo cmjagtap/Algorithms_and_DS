e008d9ccd3082d833bf731d1d4ed453fb860c277
cmjagtap <cmjagtap1@gmail.com>
2017-10-15 23:02:38 +0530

    added fibonacci

--- dynamic_programming/fibbonacci.py
+++ dynamic_programming/fibbonacci.py
@@ -2,14 +2,15 @@ def fibbo(t1,t2,no):
 	fib=[t1,t2]
 	for i in range(2,no+1):
 		fib.append(fib[i-1]+fib[i-2])
-	print fib[no]
+	return fib[no]
 t1,t2,no=map(int,raw_input().split())
-fibbo(t1,t2,no)
+print "fibbonaci",fibbo(t1,t2,no)
 
 
-# def fibbo(no):
-# 	fib=[0,1]
-# 	for i in range(2,no+1):
-# 		fib.append(fib[i-1]+fib[i-2])
-# 	return fib[no]
-# print "fib no",fibbo(3)
+def fibbo(no):
+	fib=[0,1]
+	for i in range(2,no+1):
+		fib.append(fib[i-1]+fib[i-2])
+	return fib[no]
+
+print "fib no",fibbo(3)
